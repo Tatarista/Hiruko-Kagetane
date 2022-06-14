1526a13f8094a1115a8f15740e801c46fb20ef67
Tatarista <105756147+Tatarista@users.noreply.github.com>
2022-06-15 01:00:02 +0800

    Add files via upload

--- ryudata/sewa.js
+++ ryudata/sewa.js
@@ -2,14 +2,14 @@ exports.sewa = () => {
 	return`
 *SEWA BOT 💫*
 
-📆 1 Minggu : Rp. 10.000,00
-📆 1 Bulan : Rp. 20.000,00
+📆 1 Minggu : Rp. 5.000,00
+📆 1 Bulan : Rp. 10.000,00
 📆 1 Tahun : Rp. 100.000,00
 📆 Permanen : Buat Bot Sendiri!!!
 
 *🏦 PAYMENT :*
-- Gopay
-- Dana
-- Paypal
+- 
+- PULSA
+- 
 `
 }
